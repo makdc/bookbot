bf78cd18b586686b82a1830cb97078b4ee4144fc
Malik <113217816+makdc@users.noreply.github.com>
2025-04-14 16:29:29 -0700

    Returns a dictionary of characters and their counts.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from stats import word_count
+from stats import indiv_char_count
+from stats import character_count
+from stats import get_book_text
+from stats import sorting
 
 def main():
 
-    num_words = str(word_count('./books/frankenstein.txt'))
-    output = num_words + " words found in the document"
-    print(output)
+    #num_words = str(word_count('./books/frankenstein.txt'))
+    #output = num_words + " words found in the document"
+    #print(output)
+    #content = get_book_text('./books/frankenstein.txt')
+
+
+#    num_words = str(word_count('./books/frankenstein.txt'))
+#    print(str(num_words))
+
+ #   num_char = str(character_count('./books/frankenstein.txt'))
+ #   print(str(num_char))
+
+    indiv_num_char = str(indiv_char_count('./books/frankenstein.txt'))
+    print(sorting(indiv_num_char))
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,62 @@ def word_count(f):
     w_count = len(word_list)
     return w_count
 
+def character_count(f):
+    char_list = list(get_book_text(f))
+    c_count = len(char_list)
+    return c_count
+
+def indiv_char_count(f):
+    #c_count = character_count(f)
+    dict_count = {}
+    char_list = list(get_book_text(f).lower())
+    
+    for char in char_list:
+        dict_count[char] = dict_count.get(char,0)+1
+    #print(dict_count)
+    return dict_count
+
+def ignore_this_one___indiv_char_count(f):
+    c_count = character_count(f)
+    #char_list = list(get_book_text(f))
+    dict_counts = {'char','count'}
+    char_list = list(get_book_text(f))
+    #c_count = len(char_list)
+    i = 0
+    for i in range(c_count):
+        char_value = char_list[i]
+        print(char_value)
+        is_char_present = char_value in dict_counts 
+        print(is_char_present)
+
+        if char_value not in dict_counts:
+            dict_counts[char_value] = 1 
+        
+            #is_char_present == False:
+            #dict_counts[char_value] = 1
+            #print(dict_counts)
+        #i += 1
+
+    print(dict_counts)
+    return char_list
+
+def sorting(dict):
+    new_dict = {}
+    new_dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])}
+    return new_dict
+
+
+def char_present(char):
+    is_char_present = char in characters
+    return is_char_present 
+
 def get_book_text(f):
-    f = open(f)
-    file_contents = f.read()
-    return file_contents
+    with open(f) as f:
+        # do something with f (the file) here
+        file_contents = f.read()
+        return file_contents
+
+#def split_by_char(text, chunksize):
+#    return [text[i:(i+chunksize)] for i in range(len(text)-chunksize+1)]
+
+
